cc499b8b1460946c9cfa5e92513701a390dc7fe6
mohammed.munais <mohammed.munais@nestgroup.net>
2025-04-23 12:01:57 +0530

    added orgin

--- server/app/main.py
+++ server/app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://192.168.1.4:3000"],
+    allow_origins=[ "https://jwtfastapi.vercel.app","http://localhost:3000", "http://192.168.1.4:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
